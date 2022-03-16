9fb2f6b550f980f46306c80739f58b4f786415d4
khleeltori <101099351+khleeltori@users.noreply.github.com>
2022-03-16 08:25:19 +0200

    Update actions.js

--- actions.js
+++ actions.js
@@ -3,9 +3,15 @@ const router = express.Router();
 
 router.get('/greeting',(req,res) => {
     return res.status(200).json({
-        message:'Welcome to my api'
+        message:"Welcome to my api"
+        
     });
-})
-
+});
+router.get('/ff',(req,res) => {
+    return res.status(200).json({
+        message:10*10
+        
+    });
+});
 
 module.exports = router;// هي اهم نقطة في بناء الموقع ولا يجب نسيها
