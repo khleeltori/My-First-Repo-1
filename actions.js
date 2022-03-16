c767cd90e6c62a6fb8871da7799a389f000ad912
khleeltori <101099351+khleeltori@users.noreply.github.com>
2022-03-16 10:35:12 +0200

    Update actions.js

--- actions.js
+++ actions.js
@@ -1,5 +1,21 @@
 const express = require('express');
+const req = require('express/lib/request');
+const res = require('express/lib/response');
 const router = express.Router();
+
+let users =[];
+
+router.post('/register',(req,res)=> {
+   // const user_email =req.body.email;
+   // const user_password =req.body.password;
+const{email,password} =req.body;
+
+users.push({email:email,password:password});
+    return res.status(200).json({
+        message:users
+
+}); 
+})
 
 router.get('/greeting',(req,res) => {
     return res.status(200).json({
@@ -14,4 +30,4 @@ router.get('/ff',(req,res) => {
     });
 });
 
-module.exports = router;// هي اهم نقطة في بناء الموقع ولا يجب نسيها
+module.exports = router;  //هي اهم نقطة في بناء الموقع ولا يجب نسيها
